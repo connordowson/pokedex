d081747f5beefb926026ea8a34699e2a2c9882ed
Connor Dowson <connormwdowson@gmail.com>
2018-01-02 21:23:01 +0000

    Added api calls

---
+++ src/js/App.js
@@ -0,0 +1,152 @@
+import React, { Component } from "react";
+
+function capitalise(string) 
+{
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+let pokemonStyle = {
+    display: "inline-block",
+    padding: "20px"
+};
+
+let typeStyle = {
+    display: "inline-block",
+    padding: "5px",
+    borderRadius: "2px",
+    margin: "0 5px 0 5px"
+};
+
+let typeColors = {
+
+    bug: "#A8B820",
+    dark: "#705848",
+    dragon: "#7038F8",
+    electric: "#F8D030",
+    fight: "#C03028",
+    fire: "#F08030",
+    flying: "#A890F0",
+    ghost: "#705898",
+    grass: "#78C850",
+    ground: "#E0C068",
+    ice: "#98D8D8",
+    normal: "#A8A878",
+    poison: "#A040A0",
+    psychic: "#F85888",
+    rock: "#B8A038",
+    steel: "#B8B8D0",
+    water: "#6890F0"
+
+}
+
+const urlForPokemon = pokedexNo => `https://pokeapi.co/api/v2/pokemon/${pokedexNo}`
+
+
+class SearchBar extends Component {
+
+    render(){
+        
+        return (
+                
+            <div>
+
+                <input type="search" />
+
+            </div>
+        
+        );
+
+        
+    }
+
+}
+
+class Pokemon extends Component {
+
+    constructor(props){
+
+        super(props)
+        this.state = {}
+    }
+
+    componentDidMount(){
+
+        fetch(urlForPokemon(this.props.pokedexNo))
+        .then(d => d.json())
+        .then(d => {
+
+            this.setState({
+
+                pokemonData: d,
+                type1: d.types[0].type.name
+
+            })
+
+        })
+
+    }
+    
+        render(){
+
+            if(!this.state.pokemonData) return <p style={pokemonStyle}> Loading... </p>
+
+            if(!this.state.pokemonData.types[1]) return (
+
+                    
+                <div style={pokemonStyle}>
+    
+                    <img src={this.state.pokemonData.sprites.front_default}/>
+                    <h3> {capitalise(this.state.pokemonData.name)} </h3>
+                    <div style={typeStyle} style={{backgroundColor: typeColors.grass}} className="type"> {capitalise(this.state.pokemonData.types[0].type.name)} </div>
+    
+                </div>
+            
+            );
+
+            
+            return (
+
+                    
+                <div style={pokemonStyle}>
+    
+                    <img src={this.state.pokemonData.sprites.front_default}/>
+                    <h3> {capitalise(this.state.pokemonData.name)} </h3>
+                    <div style={typeStyle}  className="type"> {capitalise(this.state.pokemonData.types[1].type.name)} </div>
+                    <div style={typeStyle}  className="type"> {capitalise(this.state.pokemonData.types[0].type.name)} </div>
+    
+                </div>
+            
+            );
+    
+            
+        }
+    
+    }
+
+class App extends Component {
+
+    render(){
+
+        return (
+            <div id="app">
+                <h1> Pokédex </h1>
+                <SearchBar />
+                <Pokemon pokedexNo={"1"} />
+                <Pokemon pokedexNo={"2"} />
+                <Pokemon pokedexNo={"3"} />
+                <Pokemon pokedexNo={"4"} />
+                <Pokemon pokedexNo={"5"} />
+                <Pokemon pokedexNo={"6"} />
+                <Pokemon pokedexNo={"7"} />
+                <Pokemon pokedexNo={"8"} />
+                <Pokemon pokedexNo={"9"} />
+            </div>
+
+        );
+
+
+    }
+
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+var path = require('path');
+var webpack = require('webpack');
+
+
+module.exports = {
+
+    entry: "./src/js/index.js",
+
+    output: {
+
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+
+    },
+
+    module: {
+
+        rules: [
+            
+            {
+
+                test: /\.scss$/,
+
+                use: [{
+
+                    loader: "style-loader" // creates style nodes from JS strings
+                },
+
+                {
+                    loader: "css-loader" // translates CSS into CommonJS
+                },
+
+                {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }]
+
+            },
+
+            {
+
+                test: /\.js$/,
+
+                exclude: /(node_modules)/,
+
+                use: {
+
+                    loader: "babel-loader",
+
+                    options: {
+
+                        presets: ['es2015', 'react']
+                        
+                    }
+
+                }
+
+            },
+
+
+            {
+                test: /\.(gif|png|jpe?g)$/i,
+
+                use: {
+
+                    loader: "file-loader",
+
+                    options: {
+
+                        name: "./assets/[name].[ext]"
+
+                    }
+                }
+
+            }
+
+        ]
+    }
+
+}
